56117412d190e8c2a9920791f1e036872783b376
Marcin <marcin.jachim.oficjalnie@gmail.com>
2023-03-28 23:48:44 +0200

    Coded fizzBuzz function

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -1,5 +1,16 @@
-function fizzBuzz() {
-
+function fizzBuzz(number) {
+    if (number%3 === 0 && number%5 === 0) {
+        return "FizzBuzz";
+    } 
+    else if (number%3 === 0 && number%5 != 0) {
+        return "Fizz";
+    }
+    else if (number%3 != 0 && number%5 === 0) {
+        return "Buzz";
+    }
+    else if (number%3 != 0 && number%5 != 0) {
+        return number;
+    }
 }
 
 module.exports = fizzBuzz;
